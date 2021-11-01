668b6fadb9e399a99326934bcb63ed1cb679122e
Greip <timurburenkov@gmail.com>
2021-11-01 23:46:35 +0200

    commit 0.1

--- Qt5Py_Test_.py
+++ Qt5Py_Test_.py
@@ -1,1 +1,11 @@
+import sys
+#import PyQt5
+from PyQt5 import QtWidgets, uic
 
+#pyuic5 Test(01.11).ui -o Test(01.11).py
+
+app = QtWidgets.QApplication([])
+win = uic.loadUi("D:\Qt5Py\Test(01.11).ui")
+
+win.show()
+sys.exit(app.exec())
