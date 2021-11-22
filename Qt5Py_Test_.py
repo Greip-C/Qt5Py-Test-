121ff408529b079b4a6f1a95003230d1f6c527cc
Greip <timurburenkov@gmail.com>
2021-11-22 10:30:23 +0200

    02

--- Qt5Py_Test_.py
+++ Qt5Py_Test_.py
@@ -1,11 +1,24 @@
 import sys
-#import PyQt5
+from mydesign import Ui_MainWindow
 from PyQt5 import QtWidgets, uic
 
 #pyuic5 Test(01.11).ui -o Test(01.11).py
 
-app = QtWidgets.QApplication([])
-win = uic.loadUi("D:\Qt5Py\Test(01.11).ui")
+#app = QtWidgets.QApplication([])
+#win = uic.loadUi("D:\Qt5Py\Test(01.11).ui")
 
-win.show()
+#win.show()
+#sys.exit(app.exec())
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+ 
+ 
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+ 
 sys.exit(app.exec())
